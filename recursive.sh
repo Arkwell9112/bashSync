62c20a78989015422ea05b99c8d6e78329b03c84
Edward <edward@edsdev.com>
2020-05-19 19:32:13 +0200

    Recursive no conflict

--- recursive.sh
+++ recursive.sh
@@ -9,35 +9,39 @@ while read file; do
         IFS='|'
         read -a tab <<< ${entry}
         if [[ ${tab[0]} == 0 ]]; then
-            ./ecriture $3$1 ${size} ${admin} ${date}
-            echo 'Bonjour les gars'
-        elif [[ ${tab[1]} == ${size} && ${tab[2]} == ${admin} && ${tab[3]} == ${date} ]]; then
+            ./ecriture $3${file} ${size} ${admin} ${date}
+            ./recursive.sh $1 $2 $3
+        elif [[ ${tab[2]} == ${size} && ${tab[3]} == ${admin} && ${tab[4]} == ${date} ]]; then
             if [[ -f $2$3${file} ]]; then
                 othersize=$(stat -c %s $2$3${file})
                 otheradmin=$(stat -c %a $2$3${file})
                 otherdate=$(stat -c %Y $2$3${file})
-                if [[ ${tab[1]} != ${othersize} || ${tab[2]} != ${otheradmin} || ${tab[3]} != ${otherdate} ]]; then
-                    mv $2$3${file} $1$3${file}
-                    ./ecriture $3${file} ${othersize} ${otheradmin} ${otherdate}
+                if [[ ${tab[2]} != ${othersize} || ${tab[3]} != ${otheradmin} || ${tab[4]} != ${otherdate} ]]; then
+                    mkdir -p $1$3 && cp $2$3${file} $1$3${file}
+                    touch $2$3${file}
+                    ./ecriture $3${file} ${othersize} ${otheradmin} $(stat -c %Y $2$3${file})
                 fi
             else
-                mv $1$3${file} $2$3${file}
-                ./ecriture $3${file} ${size} ${admin} ${date}
+                mkdir -p $2$3 && cp $1$3${file} $2$3${file}
+                touch $1$3${file}
+                ./ecriture $3${file} ${size} ${admin} $(stat -c %Y $1$3${file})
             fi
         else
             if [[ -f $2$3${file} ]]; then
                 othersize=$(stat -c %s $2$3${file})
                 otheradmin=$(stat -c %a $2$3${file})
                 otherdate=$(stat -c %Y $2$3${file})
-                if [[ ${tab[1]} == ${othersize} && ${tab[2]} == ${otheradmin} && ${tab[3]} == ${otherdate} ]]; then
-                    mv $1$3${file} $2$1${file}
-                    ./ecriture $3${file} ${size} ${admin} ${date}
+                if [[ ${tab[2]} == ${othersize} && ${tab[3]} == ${otheradmin} && ${tab[4]} == ${otherdate} ]]; then
+                    mkdir -p $2$3 && cp $1$3${file} $2$3${file}
+                    touch $1$3${file}
+                    ./ecriture $3${file} ${size} ${admin} $(stat -c %Y $1$3${file})
                 else
                     echo 'Conflict'
                 fi
             else
-                mv $1$3${file} $2$1${file}
-                ./ecriture $3${file} ${size} ${admin} ${date}
+                mkdir -p  $2$3 && cp $1$3${file} $2$3${file}
+                touch $1$3${file}
+                ./ecriture $3${file} ${size} ${admin} $(stat -c %Y $1$3${file})
             fi
         fi
     elif [[ -d $1$3${file} ]]; then
