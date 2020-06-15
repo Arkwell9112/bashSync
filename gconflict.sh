7a22a40aff4e916b876b88ebf5baebe77b0f1c14
Deussandouze <florianbourrier@gmail.com>
2020-06-15 14:34:27 +0200

    oui

--- gconflict.sh
+++ gconflict.sh
@@ -2,6 +2,7 @@
 
 diff -u $1 $2
 echo "Veuillez entrez - si vous voulez que le fichier représenté par + soit écrasè ou inversement."
+echo "Dans le cas des fichiers binaires, le fichier représenté par - est le premier fichier listé."
 while read -p "+/- ?: " answer </dev/tty; do
     case ${answer} in
     -)
